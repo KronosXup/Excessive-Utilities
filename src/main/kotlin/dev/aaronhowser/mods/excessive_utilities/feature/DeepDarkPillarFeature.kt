39cbac3f80fa706746f21aa8706448155b5466b2
aaron <aaronhowser1@gmail.com>
2026-06-09 14:37:57 -0500

    feature

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/feature/DeepDarkPillarFeature.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/feature/DeepDarkPillarFeature.kt
@@ -1,11 +1,13 @@
 package dev.aaronhowser.mods.excessive_utilities.feature
 
 import net.minecraft.core.BlockPos
+import net.minecraft.util.Mth
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.levelgen.feature.Feature
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext
 import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration
+import kotlin.math.sqrt
 
 class DeepDarkPillarFeature : Feature<NoneFeatureConfiguration>(NoneFeatureConfiguration.CODEC) {
 
@@ -13,13 +15,37 @@ class DeepDarkPillarFeature : Feature<NoneFeatureConfiguration>(NoneFeatureConfi
 		val origin = context.origin()
 		val level = context.level()
 
-		val volume = BlockPos.betweenClosed(
-			origin.offset(-5, -100, -5),
-			origin.offset(5, 100, 5),
-		)
+		val height = 200
+		val minRadius = 15
+		val maxRadius = 25
 
-		for (pos in volume) {
-			level.setBlock(pos, Blocks.STONE.defaultBlockState(), Block.UPDATE_ALL_IMMEDIATE)
+		val mutable = BlockPos.MutableBlockPos()
+
+		val centerX = origin.x
+		val centerZ = origin.z
+
+		for (dy in 0 until height) {
+			val y = origin.y + dy
+
+			val percentUp = dy.toDouble() / height
+			val radius = Mth.lerp(percentUp, minRadius.toDouble(), maxRadius.toDouble())
+
+			val maxRad = Mth.ceil(radius)
+
+			for (dx in -maxRad..maxRad) {
+				val x = centerX + dx
+
+				for (dz in -maxRad..maxRad) {
+					val dist = sqrt((dx * dx.toFloat()) + (dz * dz))
+					if (dist > radius) continue
+
+					val z = centerZ + dz
+
+					mutable.set(x, y, z)
+					level.setBlock(mutable, Blocks.COBBLESTONE.defaultBlockState(), Block.UPDATE_CLIENTS)
+				}
+			}
+
 		}
 
 		return true
