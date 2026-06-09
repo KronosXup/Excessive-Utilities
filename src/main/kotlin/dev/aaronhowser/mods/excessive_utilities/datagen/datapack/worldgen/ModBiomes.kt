0e8b77f96e91ff93782484a6e5bf5042a2363c27
aaron <aaronhowser1@gmail.com>
2026-06-09 14:57:45 -0500

    add features and ore generation to Deep Dark biome

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/datagen/datapack/worldgen/ModBiomes.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/datagen/datapack/worldgen/ModBiomes.kt
@@ -2,18 +2,34 @@ package dev.aaronhowser.mods.excessive_utilities.datagen.datapack.worldgen
 
 import dev.aaronhowser.mods.excessive_utilities.ExcessiveUtilities
 import net.minecraft.core.registries.Registries
+import net.minecraft.data.worldgen.BiomeDefaultFeatures
 import net.minecraft.data.worldgen.BootstrapContext
+import net.minecraft.data.worldgen.features.OreFeatures
+import net.minecraft.data.worldgen.placement.OrePlacements
 import net.minecraft.resources.ResourceKey
 import net.minecraft.world.level.biome.Biome
 import net.minecraft.world.level.biome.BiomeGenerationSettings
 import net.minecraft.world.level.biome.BiomeSpecialEffects
 import net.minecraft.world.level.biome.MobSpawnSettings
+import net.minecraft.world.level.levelgen.GenerationStep
 
 object ModBiomes {
 
 	val DEEP_DARK = rk("deep_dark")
 
 	fun bootstrap(context: BootstrapContext<Biome>) {
+
+		val placedFeatures = context.lookup(Registries.PLACED_FEATURE)
+		val configuredCarvers = context.lookup(Registries.CONFIGURED_CARVER)
+
+		val biomeGenerationSettingsBuilder = BiomeGenerationSettings.Builder(
+			placedFeatures,
+			configuredCarvers
+		)
+
+		BiomeDefaultFeatures.addDefaultOres(biomeGenerationSettingsBuilder)
+		BiomeDefaultFeatures.addFossilDecoration(biomeGenerationSettingsBuilder)
+
 		val biome = Biome.BiomeBuilder()
 			.hasPrecipitation(false)
 			.temperature(0.8f)
@@ -30,10 +46,7 @@ object ModBiomes {
 				MobSpawnSettings.Builder().build()
 			)
 			.generationSettings(
-				BiomeGenerationSettings.Builder(
-					context.lookup(Registries.PLACED_FEATURE),
-					context.lookup(Registries.CONFIGURED_CARVER)
-				).build()
+				biomeGenerationSettingsBuilder.build()
 			)
 			.build()
 
